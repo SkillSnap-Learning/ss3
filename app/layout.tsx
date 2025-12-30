450ec5013f686ba2d36f57f1c8ce99ed4b52332f
saquelain <saquelain1502@gmail.com>
2025-12-30 23:42:07 +0530

    poppin font added

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserrat.variable} font-montserrat antialiased`}
+        className={poppins.className}
       >
         {children}
       </body>
